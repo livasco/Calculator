215785116696c058534fcdae13bb145f344222ce
livasco <liviuivasco13@gmail.com>
2021-05-22 14:38:44 +0300

    caculator initial

---
+++ MJS.js
@@ -0,0 +1,9 @@
+function HCB(value) {
+	console.log (value)
+      document.getElementsByClassName("output").innerHTML = "value";
+}
+
+function clearValue() {
+	document.GetElementsByClass('output').innerHTML = 0
+	console.log (1111)
+}	
